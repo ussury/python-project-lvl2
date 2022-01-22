a131eda70d3c4a33fc8338abfd5fa6c8558381d3
ussury <meshalkins@ya.ru>
2022-01-22 15:15:27 +0300

    шаг второй

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,6 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('first_file', type=str)
 parser.add_argument('second_file', type=str)
+parser.add_argument('-f', '--format', type=str, help='set format of output')
 
 args = parser.parse_args()
 
