d2e35b22fbb80e6695c70f77ffd4151d25a6634d
ussury <meshalkins@ya.ru>
2022-05-04 19:22:09 +0300

    add default=stylish

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,11 +4,16 @@ from gendiff.diff import generate_diff
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', type=str,
-                        help='set format of output')
+    parser = argparse.ArgumentParser(
+        prog='gendiff', 
+        usage='%(prog)s [options] <filepath1> <filepath2>',
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        default='stylish',
+        help='output format (default: "stylish")')
     args = parser.parse_args()
 
     return args.first_file, args.second_file
