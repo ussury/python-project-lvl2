64b5446cbda319684c93668d631e3c953a1451d1
ussury <meshalkins@ya.ru>
2022-05-06 22:06:13 +0300

    перенес argparse в cli.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python3
-import argparse
+from gendiff.cli import get_args
 from gendiff.diff import generate_diff
-
-
-def get_args():
-    parser = argparse.ArgumentParser(
-        prog='gendiff',
-        usage='%(prog)s [options] <filepath1> <filepath2>',
-        description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument(
-        '-f', '--format',
-        default='stylish', type=str,
-        help='output format (default: "stylish")')
-    args = parser.parse_args()
-
-    return args.first_file, args.second_file, args.format
 
 
 def main():
